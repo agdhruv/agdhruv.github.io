48b6e1a07e99da72468eeb8cb7621f7bd4b83633
agdhruv <dhruv_agarwal@live.com>
2017-12-26 16:00:21 +0530

    Work section added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,29 @@
+// GET projects.json file from the server
+$.getJSON('/js/projects.json', (data) => {
+
+	// get projects from JSON file into specific variable 
+	var projects = data['projects'];
+
+	// populate the page by iterating through the ex-JSON object 
+	for (var i = 0; i < projects.length; i++) {
+
+		// init string
+		var HTMLString = '';
+
+		// generate HTML string to append from ex-JSON Object
+		HTMLString += '<article class="project">';
+		HTMLString += '<h2 class="name">' + projects[i]['name'] + '</h2>';
+		HTMLString += '<p class="stack">Stack: ' + projects[i]['stack'] + '</p>';
+		HTMLString += '<p class="description">' + projects[i]['description'] + '</p>';
+		// HTMLString += '<a target="_blank" href="' + projects[i]['link'] + '" class="link">Link</a>';
+		HTMLString += '</article>';
+
+		// append generated HTML string to respective element
+		$('.my-projects').append(HTMLString);
+	}
+});
+	
+// rest of the DOM logic in a self-invoking anonymour function to avoid global variables
 (function() {
 
 	$('.scroll-chevron').click(function() {
