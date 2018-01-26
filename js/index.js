c67ce462eb1772ef6706430314c11b7d48844ce2
agdhruv <dhruv_agarwal@live.com>
2018-01-27 02:58:22 +0530

    Made major changes to the UI and UX of the page, reduced text and added images

--- js/index.js
+++ js/index.js
@@ -11,12 +11,18 @@ $.getJSON('/js/projects.json', (data) => {
 		var HTMLString = '';
 
 		// generate HTML string to append from ex-JSON Object
-		HTMLString += '<article class="project" data-project-no="' + i + '">';
-		HTMLString += '<h2 class="name">' + projects[i]['name'] + '</h2>';
-		HTMLString += '<p class="stack">Stack: ' + projects[i]['stack'] + '</p>';
-		HTMLString += '<p class="description">' + projects[i]['description'] + '</p>';
-		HTMLString += '<a target="_blank" href="' + projects[i]['link'] + '" class="link">Link</a>';
-		HTMLString += '</article>';
+		HTMLString += `<article class="project" data-project-no="${i}">`;
+		HTMLString += `<h2 class="thumbnail" style="background-image: url('${projects[i]['thumbnail']}')"></h2>`;
+		if (projects[i]['link']) {
+			HTMLString += `<h2 class="name">${projects[i]['name']}<a target="_blank" href="${projects[i]['link']}" class="link"><i class="fas fa-external-link-alt" aria-hidden="true"></i></a></h2>`;
+		}
+		else {
+			HTMLString += `<h2 class="name">${projects[i]['name']}</h2>`;
+		}
+		HTMLString += `<p class="stack">Stack: ${projects[i]['stack']}</p>`;
+		HTMLString += `<p class="description get-ellipsis">${projects[i]['description']}</p>`;
+		HTMLString += `<button class="expand"><i class="fas fa-plus-circle" aria-hidden="true"></i></button>`;
+		HTMLString += `</article>`;
 
 		// append generated HTML string to respective element
 		$('.my-projects').append(HTMLString);
@@ -32,4 +38,9 @@ $.getJSON('/js/projects.json', (data) => {
 		}, 500);
 	});
 
+	$(document).on('click', 'article.project button.expand i', function() {
+		var $this = $(this);
+		$this.closest('button.expand').toggleClass('red');
+		$this.closest('.project').find('.description').toggleClass('get-ellipsis');
+	});
 })();
